bad00c02d1f12f2bfa4ca8db602cb31bd364aa8c
Cat Luong <luongcat162002@gmail.com>
2024-09-08 20:51:59 -0400

    Initial commit - Assignment 1

---
+++ assignment_1/code/morphology.py
@@ -0,0 +1,15 @@
+class Morphology: 
+    def __init__(self): 
+        pass
+
+    def read_files(self): 
+        pass
+    
+    def dfs(self): 
+        pass
+
+    def print_results(self): 
+        pass
+
+
+
---
+++ assignment_1/scripts/grading.sh
@@ -0,0 +1,29 @@
+echo '####################'
+echo '#  Grading Script  #'
+echo '####################'
+echo
+
+scoring=false
+python3 morphology.py Dict0.txt Rules0.txt Test0.txt > yourTrace.txt
+
+if $scoring; then
+    missing=$( comm -13 <(sed '/^$/d' yourTrace.txt | sort ) \
+                        <(sed '/^$/d' Trace0.txt | sort ) | wc -l )
+    extra=$( comm -23 <(sed '/^$/d' yourTrace.txt | sort ) \
+                      <(sed '/^$/d' Trace0.txt | sort ) | wc -l )
+    t1=$( sed '/^$/d' Trace0.txt | wc -l )
+    t2=$( sed '/^$/d' yourTrace.txt | wc -l )
+    echo "Missing Derivations:" $missing
+    echo "Extra Derivations:" $extra
+    if [[ $missing -eq "0" && $extra -eq "0" ]]; then
+        echo "Correct!"
+    fi
+else
+    ds=$( diff -y yourTrace.txt Trace0.txt )
+    if [ -z "$ds" ]; then
+        echo "Correct!"
+    else
+        echo "There are some errors!"
+        diff -y yourTrace.txt Trace0.txt
+    fi
+fi
